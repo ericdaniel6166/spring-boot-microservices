dc760db3d5a77259913da2865105481fae8f0e35
eric6166 <ericnguyen6166@gmail.com>
2023-08-27 15:29:00 +0700

    feature/SM-0007

---
+++ spring-boot-microservices-framework/src/test/java/com/example/springbootmicroservicesframework/SpringBootMicroservicesFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmicroservicesframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservicesFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
