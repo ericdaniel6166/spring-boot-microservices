9deac4fe86bb6559fbc17e1bb600abc8dbda0a98
eric6166 <ericnguyen6166@gmail.com>
2023-10-11 20:59:03 +0700

    feature/SM-0030

---
+++ spring-boot-microservices-framework/src/main/java/com/example/springbootmicroservicesframework/kafka/event/Event.java
@@ -0,0 +1,24 @@
+package com.example.springbootmicroservicesframework.kafka.event;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PROTECTED)
+public class Event implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 37397013697L;
+
+    Object payload;
+}
